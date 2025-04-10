b57d1447a6e064131c53481ffa5279792c7cade8
Pushmenkov Sergey <sergeyemail>
2025-04-10 13:46:33 +0000

    Сгенерировал модель, создал миграции

--- main-api/db/schema.rb
+++ main-api/db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_10_105610) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_10_134459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "event_name"
+    t.date "event_date"
+    t.integer "base_tickets_amount"
+    t.integer "vip_tickets_amount"
+    t.float "base_ticket_price"
+    t.float "vip_ticket_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
