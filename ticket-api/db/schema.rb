06c6a0266580a2e5a2b476b2c1492b8ef533a06a
Pushmenkov Sergey <sergeyemail>
2025-04-11 11:41:05 +0000

    Поменял status на int, category на string

--- ticket-api/db/schema.rb
+++ ticket-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_11_113434) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_11_113838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,8 +41,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_11_113434) do
   end
 
   create_table "tickets", force: :cascade do |t|
-    t.integer "category"
-    t.string "status"
+    t.string "category"
+    t.integer "status"
     t.decimal "price"
     t.string "event"
     t.date "event_date"
