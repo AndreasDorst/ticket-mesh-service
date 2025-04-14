c54f21b4c62de66e181edb1998198ce29b22357e
Andrey Kharlamov <148-ruby-intern-9@users.noreply.172.24.100.179>
2025-04-14 14:46:48 +0000

    Create basic functional

--- main-api/app/controllers/events_controller.rb
+++ main-api/app/controllers/events_controller.rb
@@ -1,17 +1,54 @@
 class EventsController < ApplicationController
-  def create
-    event = Event.new(event_params)
+  class TicketServiceError < StandardError; end
 
-    if event.save
-      render json: event, status: :created
-    else
-      render json: { errors: event.errors.full_messages }, status: :unprocessable_entity
+  def create
+    Rails.logger.info "Received parameters: #{params.inspect}"
+
+    @event = nil
+
+    ActiveRecord::Base.transaction do
+      @event = Event.create!(event_params)
+
+      Rails.logger.info "Created event: #{@event.inspect}, ID: #{@event.id}"
+
+      url = "#{MICROSERVICES::TICKET_SERVICE}/api/ticket/bulk_create"
+      headers = { 'Content-Type' => 'application/json' }
+      body = {
+        event_id: @event.id,
+        base_tickets_count: @event.base_tickets_amount,
+        vip_tickets_count: @event.vip_tickets_amount,
+        base_price: @event.base_ticket_price,
+        vip_price: @event.vip_ticket_price
+      }.to_json
+
+      response = Faraday.post(url, body, headers)
+
+      unless response.status == 201
+        raise TicketServiceError, response.body
+      end
     end
+
+    render json: { success: true, event_id: @event.id }
+
+  rescue Faraday::ConnectionFailed => e
+    Rails.logger.error "Ticket Service connection failed: #{e.message}"
+    render json: { error: "Service unavailable" }, status: :service_unavailable
+
+  rescue TicketServiceError => e
+    Rails.logger.error "Rollback (TicketServiceError): #{e.message}"
+    render json: { error: "Failed to create event (Ticket Service Error)" }, status: :unprocessable_entity
+
+  rescue => e
+    Rails.logger.error "Unexpected error: #{e.message}"
+    render json: { error: "Unexpected error" }, status: :unprocessable_entity
   end
 
   private
 
   def event_params
-    params.require(:event).permit(:event_name, :event_date, :base_tickets_amount, :vip_tickets_amount, :base_ticket_price, :vip_ticket_price)
+    params.require(:event).permit(
+      :event_name, :event_date, :base_tickets_amount,
+      :vip_tickets_amount, :base_ticket_price, :vip_ticket_price
+    )
   end
 end
--- main-api/config/initializers/microservices_base_urls.rb
+++
@@ -1,4 +0,0 @@
-module MICROSERVICES
-  TICKET_SERVICE = 'http://ticket-api:3000'
-   # Добавляйте другие микросервисы по мере необходимости
-end
